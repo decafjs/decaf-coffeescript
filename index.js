b36365cd1d31808bbf1dae3c83a1563adeb98278
Michael Schwartz <mykesx@gmail.com>
2014-05-21 08:42:48 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: mschwartz
+ * Date: 6/12/13
+ * Time: 3:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+/*global require, include */
+(function() {
+    "use strict";
+
+    var fs = require('fs'),
+        rhino = require('builtin/rhino'),
+        me = {};
+
+    include.call(me, require.fsPath + 'lib/coffee-script.js');
+    include.path.push('./coffeescript');
+    require.path.push('./coffeescript');
+    require.extensions.coffee = include.extensions.coffee = function(fn) {
+        return rhino.runScript(me.CoffeeScript.compile(fs.readFile(fn), { bare: true }), fn, 1, this);
+    };
+    exports.CoffeeScript = me.CoffeeScript;
+
+}());
